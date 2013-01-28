198009d2329f21d005fc0e037aaf226d955b7c1a
Mozillazg <opensource.mozillazg@gmail.com>
2013-01-28 22:27:53 +0800

    update

--- projects/bbs/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib improt admin
-from app.models import Node
-from app.models import Topic
-from app.models import Reply
-
-admin.site.register(Node)
-admin.site.register(Topic)
-admin.site.register(Relpy)
